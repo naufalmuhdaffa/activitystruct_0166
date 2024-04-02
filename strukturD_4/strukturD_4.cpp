b6c2ab6f38e2be872298cae5abf0ec86b4c26f92
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-04-02 11:42:43 +0700

    menambahkan input dan output

--- strukturD_4/strukturD_4.cpp
+++ strukturD_4/strukturD_4.cpp
@@ -22,5 +22,15 @@ int main(){
         getline(cin, mhs[i].nim);
         cout << "Nama Mahasiswa : ";
         getline(cin, mhs[i].nama);
+
+        cout << "Alamat Mahasiswa : " << endl;
+        cout << "\t Nama Desa : ";
+        getline(cin, mhs[i].alamat.desa);
+        cout << "\t Nama Kota: ";
+        getline(cin, mhs[i].alamat.kota);
+        cout << "Umur Mahasiswa : ";
+        cin >> mhs[i].umur;
+        cin.ignore(1, '\n');
+        cout << endl;
     }
 }
