46fd328cb26a7804c6116600765fd090850aa0b6
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-04-02 11:42:14 +0700

    menambahkan getline input dan output

--- strukturD_4/strukturD_4.cpp
+++ strukturD_4/strukturD_4.cpp
@@ -19,5 +19,8 @@ int main(){
     for (int i = 0; i < 3; i++) {
         cout << "Data ke-" << (i + 1) << ":" << endl;
         cout << "Nomor Mahasiswa : ";
+        getline(cin, mhs[i].nim);
+        cout << "Nama Mahasiswa : ";
+        getline(cin, mhs[i].nama);
     }
 }
