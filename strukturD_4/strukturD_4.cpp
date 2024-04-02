9158d6d9261bbbb6b6db629bb3d70938ae99c913
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-04-02 11:44:29 +0700

    menambahkan output

--- strukturD_4/strukturD_4.cpp
+++ strukturD_4/strukturD_4.cpp
@@ -37,5 +37,6 @@ int main(){
         cout << endl;
         cout << "Data Mahasiswa ke-" << (i + 1) << ":" << endl;
         cout << "\n NIM : " << mhs[i].nim;
+        cout << "\n Nama : " << mhs[i].nama;
     }
 }
